32f2acd3f327185b421cc5a07d52cb797eee10aa
zeta_kabel <yaroslav.zeta@gmail.com>
2020-01-09 15:53:26 +0700

    home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,7 +26,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        dump(Auth::user()->hasRole('administrator'));
         return view('home');
     }
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,22 +5,23 @@
 
     </header>
     <main>
+        <div class="logo">ЗЭТАТехПро</div>
         <div class="home">
             <h1>Проектные решения</h1>
             <div class="links">
-                <a class="btn inverse">Каталог</a>
+                <a class="btn inverse"><img src="storage/cart.svg" alt="">Каталог</a>
                 <a href="{{route('projects')}}"
-                   class="btn inverse">
+                   class="btn inverse"><img src="storage/money.svg" alt="">
                     Бонусная <br> программа
                 </a>
                 <br>
-                <a href="/price.xlsx" class="btn">Прайс</a>
-                <a href="" class="btn">О бренде</a>
-                <a class="btn">Контакты</a>
-                <a class="btn">Техническая документация</a>
-                <a class="btn">Скачать каталог</a>
+                <a href="/price.xlsx" class="btn"><img src="storage/price.svg" alt="">Прайс</a>
+                <a href="" class="btn"><img src="storage/medal.svg" alt="">О бренде</a>
+                <a class="btn"><img src="storage/bank.svg" alt="">Контакты</a>
+                <a class="btn"><img src="storage/convert.svg" alt="">Документация</a>
+                <a class="btn"><img src="storage/download.svg" alt="">Скачать каталог</a>
             </div>
-            <div class="dekor1"></div>
+            <div class="dekor1"><img src="storage/main_logo.png" alt=""></div>
             <div class="dekor2"></div>
         </div>
 
