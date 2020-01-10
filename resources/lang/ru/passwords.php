78de399feccb18b64753d0a2a179b03e03d931ed
zeta_kabel <yaroslav.zeta@gmail.com>
2020-01-10 16:50:29 +0700

    10.01.2020

---
+++ resources/lang/ru/passwords.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Ваш пароль был сброшен!',
+    'sent' => 'Мы отправили ссылку для восстановления пароля на ваш e-mail!',
+    'token' => 'Токен сброса пароля не верный.',
+    'user' => "Пользователь с указанным e-mail не найден",
+
+];
