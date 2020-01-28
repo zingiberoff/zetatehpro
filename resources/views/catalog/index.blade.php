cf552a65d1158e75aa016695dc6b0ca9ed1173c7
zeta_kabel <yaroslav.zeta@gmail.com>
2020-01-28 10:56:07 +0700

    panic commit

--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -3,28 +3,34 @@
 
 
 
-    <div class="container catalog mt-4">
-      <left-menu></left-menu>
-        <div class="card">
-            <div class="card-header h1">{{$title??'Каталог продукции'}}</div>
-            <div class="p-4">
-                <form action="{{ url('search') }}" method="get">
-                    <div class="form-group">
-                        <input
-                                type="text"
-                                name="q"
-                                class="form-control"
-                                placeholder="Search..."
-                                value="{{ request('q') }}"
-                        />
+    <div class="container-fluid catalog mt-4">
+        <div class="row">
+            <div class="col-12 col-lg-3">
+                <left-menu></left-menu>
+            </div>
+            <div class="col-12 col-lg-9">
+                <div class="card">
+                    <div class="card-header h1">{{$title??'Каталог продукции'}}</div>
+                    <div class="p-4">
+                        <form action="{{ url('search') }}" method="get">
+                            <div class="form-group">
+                                <input
+                                        type="text"
+                                        name="q"
+                                        class="form-control"
+                                        placeholder="Search..."
+                                        value="{{ request('q') }}"
+                                />
+
+                            </div>
+                        </form>
+
+
+                        @include('catalog.list')
+
 
                     </div>
-                </form>
-
-
-                @include('catalog.list')
-
-
+                </div>
             </div>
         </div>
 
