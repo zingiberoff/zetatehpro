3c9dca3782bec146924d08fa1b60dbf50c615d2b
Alex <32@7899.ru>
2020-03-19 12:05:39 +0700

    Обновлен телефон

--- resources/views/info/contacts.blade.php
+++ resources/views/info/contacts.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         <div class="h2">Mенеджер по работе с клиентами:</div>
         <div class="h1">Филатов Вячеслав</div>
-        <div class="h2"><a href="tel:89537628786">
+        <div class="h2"><a href="tel:83832174094">
                 <v-icon>mdi-phone</v-icon>
                 +7(383)217-40-94</a></div>
         <div class="h2"><a href="mailto:29@7899.ru">
