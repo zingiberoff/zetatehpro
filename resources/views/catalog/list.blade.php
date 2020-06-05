951bfac469ad9d176a481fe39a2ee0320c8e719b
Александр Гусев <alex1232131@mail.ru>
2020-06-05 14:00:14 +0700

    Update list.blade.php

--- resources/views/catalog/list.blade.php
+++ resources/views/catalog/list.blade.php
@@ -54,12 +54,33 @@
                         {{-- @if(($product->sections[0]->id >= 28 && $product->sections[0]->id <= 34) || ($product->sections[0]->id >= 55 && $product->sections[0]->id <= 70)) --}}
                         @if(($product->sections[0]->id >= 1 && $product->sections[0]->id <= 7) || ($product->sections[0]->id >= 26 && $product->sections[0]->id <= 41))
                             {{-- Кабельные вводы --}}
-                            @foreach ($product->properties as $property)
-                                @if($property->name == 'Номин. размер резьбы метрической M/PG' || $property->name == 'Номин. размер резьбы в дюймах NPT/резьбы газовой трубы' || $property->name == 'Для взрывоопасной зоны по газу (Ex)' || $property->name == 'Степень защиты (IP)' || $property->name == 'Подходит для кабеля диаметром:' || $property->name == 'Взрывобезопасное исполнение' || $property->name == 'Шаг резьбы' || $property->name == 'Материал' || $property->name == 'Тип резьбы' || $property->name == 'Рабочая температура' || $property->name == 'Вид/марка материала' || $property->name == 'Длина резьбы')  
-                                    <b>{{$property->name}} </b>:
-                                    {{$property->value }}<br>
+                            @php
+                                $props = [];
+                                foreach($product->properties as $property) {
+                                    if($property->name == 'Шаг резьбы' || $property->name == 'Номин. размер резьбы метрической M/PG' || $property->name == 'Номин. размер резьбы в дюймах NPT/резьбы газовой трубы' || $property->name == 'Для взрывоопасной зоны по газу (Ex)' || $property->name == 'Степень защиты (IP)' || $property->name == 'Подходит для кабеля диаметром:' || $property->name == 'Взрывобезопасное исполнение' || $property->name == 'Материал' || $property->name == 'Тип резьбы' || $property->name == 'Рабочая температура' || $property->name == 'Вид/марка материала' || $property->name == 'Длина резьбы') {
+                                        $props[$property->name] = $property->value;
+                                    }
+                                }
+                            @endphp
+
+                            <b>Тип резьбы</b>:
+                            {{$props['Тип резьбы']}}<br>
+                            <b>Шаг резьбы</b>:
+                            {{$props['Шаг резьбы']}}<br>
+                            <b>Длина резьбы</b>:
+                            {{$props['Длина резьбы']}}<br>
+
+                            @foreach ($props as $name => $value)
+                                @if($name == 'Номин. размер резьбы метрической M/PG' || $name == 'Номин. размер резьбы в дюймах NPT/резьбы газовой трубы' || $name == 'Степень защиты (IP)' || $name == 'Подходит для кабеля диаметром:' || $name == 'Материал' || $name == 'Рабочая температура' || $name == 'Вид/марка материала')
+                                    <b>{{$name}} </b>:
+                                    {{$value }}<br>
                                 @endif
                             @endforeach
+                            
+                            <b>Для взрывоопасной зоны по газу (Ex)</b>:
+                            {{$props['Для взрывоопасной зоны по газу (Ex)']}}<br>
+                            <b>Взрывобезопасное исполнение</b>:
+                            {{$props['Взрывобезопасное исполнение']}}<br>
 
                         {{-- @elseif ($product->sections[0]->id >= 36 && $product->sections[0]->id <= 54) --}}
                         @elseif ($product->sections[0]->id >= 9 && $product->sections[0]->id <= 25)
@@ -85,7 +106,7 @@
                         @elseif ($product->sections[0]->id >= 8 && $product->sections[0]->id <= 8)
                             {{-- Коробки --}}
                             @foreach ($product->properties as $property)
-                                @if($property->name == 'Для взрывоопасной зоны по пыли (Ex)' || $property->name == 'Для взрывоопасной зоны по газу (Ex)' || $property->name == 'Степень защиты (IP)' || $property->name == 'Взрывобезопасное исполнение' || $property->name == 'Материал' || $property->name == 'Рабочая температура' || $property->name == 'Защитное покрытие поверхности' || $property->name == 'Цвет' || $property->name == 'Макс. поперечное сечение проводника' || $property->name == 'Крепление крышки' || $property->name == 'Количество вводов' || $property->name == 'Рабочая температура' || $property->name == 'Тип ввода' || $property->name == 'Длина' || $property->name == 'Количество соединительных зажимов/клемм' || $property->name == 'С крышкой (-ами)' || $property->name == 'Глубина' || $property->name == 'Ширина' || $property->name == 'Способ монтажа')  
+                                @if($property->name == 'Для взрывоопасной зоны по пыли (Ex)' || $property->name == 'Для взрывоопасной зоны по газу (Ex)' || $property->name == 'Степень защиты (IP)' || $property->name == 'Взрывобезопасное исполнение' || $property->name == 'Материал' || $property->name == 'Защитное покрытие поверхности' || $property->name == 'Цвет' || $property->name == 'Макс. поперечное сечение проводника' || $property->name == 'Крепление крышки' || $property->name == 'Количество вводов' || $property->name == 'Рабочая температура' || $property->name == 'Тип ввода' || $property->name == 'Длина' || $property->name == 'Количество соединительных зажимов/клемм' || $property->name == 'Глубина' || $property->name == 'Ширина' || $property->name == 'Способ монтажа')  
                                     <b>{{$property->name}} </b>:
                                     {{$property->value }}<br>
                                 @endif
@@ -93,7 +114,7 @@
                         @else
                             {{-- Муфты --}}
                             @foreach ($product->properties as $property)
-                                @if($property->name == 'Тип кабеля' || $property->name == 'Климатическое исполнение' || $property->name == 'Изготавливается по' || $property->name == 'Сертификат' || $property->name == 'Сечение кабеля, мм2' || $property->name == 'Тип муфты' || $property->name == 'Сечение кабеля (Число)' || $property->name == 'Тип установки' || $property->name == 'Рабочее напряжение' || $property->name == 'Количество жил' || $property->name == 'Наличие и тип контактных элементов' || $property->name == 'Комплект заземления' || $property->name == 'Наличие защитного слоя в кабеле' || $property->name == 'Тип изоляции' || $property->name == 'Длина разделки' || $property->name == 'Бренд')  
+                                @if($property->name == 'Артикул' || $property->name == 'Тип изделия' || $property->name == 'Вид изделия' || $property->name == 'Сечение кабеля, мм2' || $property->name == 'Тип муфты' || $property->name == 'Сечение кабеля (Число)' || $property->name == 'Тип установки' || $property->name == 'Рабочее напряжение' || $property->name == 'Количество жил' || $property->name == 'Наличие и тип контактных элементов' || $property->name == 'Комплект заземления' || $property->name == 'Наличие защитного слоя в кабеле' || $property->name == 'Тип изоляции' || $property->name == 'Тип кабеля' || $property->name == 'Наличие контактных элементов' || $property->name == 'Горючесть' || $property->name == 'Климатическое исполнение' || $property->name == 'Изготавливается по' || $property->name == 'Сертификат' || $property->name == 'Бренд')  
                                     <b>{{$property->name}} </b>:
                                     {{$property->value }}<br>
                                 @endif
